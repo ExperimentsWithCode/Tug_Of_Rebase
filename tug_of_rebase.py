874033bfb01fbc194a9a61b359bf9a066a2e8d49
Meir <meir@pop-os.localdomain>
2020-08-07 18:14:08 -0400

    Clarity

--- tug_of_rebase.py
+++ tug_of_rebase.py
@@ -23,15 +23,17 @@ class TugOfRebase():
 
     #
     def lock(self, ctx):
-        if self._isLocked(ctx):
-            return False  #?
-        self.lock_number = ctx.block_number
-        self.last_token_supply = ctx.AR[self.token_contract].total_supply
-        return True
+        # if self._isLocked(ctx):
+        #     return False  #?
+        # self.lock_number = ctx.block_number
+        # self.last_token_supply = ctx.AR[self.token_contract].total_supply
+        # return True
+        pass
     #
     def _isLocked(self, ctx):
-        last_rebase_block = ctx.AR[self.token_contract].last_rebase
-        return self.lock_number - last_rebase_block < 0
+        pass
+        # last_rebase_block = ctx.AR[self.token_contract].last_rebase
+        # return self.lock_number - last_rebase_block < 0
     #
     def unlock(self, ctx):
         # set rebase numbers
@@ -59,22 +61,31 @@ class TugOfRebase():
         return self._calcSupplyDelta(ctx) / self.last_token_supply
     #
     def _calcAdjust(self, ctx):
+        # Get Post Rebase Balance
         new_balance = ctx.AR[self.token_contract].getBalance()
+        # Calc offsets from unequal provisions
         offset_long = self.long_balance /(self.long_balance + self.short_balance)
         offset_short = self.short_balance /(self.long_balance + self.short_balance)
+        # Apply offsets
         new_long_bal = (new_balance - self.total_supply()) * offset_long + self.long_balance
         new_short_bal = (new_balance - self.total_supply()) * offset_short + self.short_balance
+        # Confirm Balances Match up
         if not self._validateAdjust(new_long_bal, new_short_bal, new_balance):
             return False
+        # Apply Rebase
         rebase_diff = self._calcRebaseDiff(ctx)
         if rebase_diff > 0: # Longs win
             new_short_shift = self.short_balance * rebase_diff
             new_short_bal -= new_short_shift
             new_long_bal += new_short_shift
-        else:  # Shorts Win
+        else:               # Shorts Win
             new_long_shift = self.long_balance * rebase_diff
             new_long_bal -= new_long_shift
             new_short_bal += new_long_shift
+        if not self._validateAdjust(new_long_bal, new_short_bal, new_balance):
+            return False
+        self.long_balance = new_long_bal
+        self.short_balance = new_short_bal
     #
     def _validateAdjust(self, long, short, new):
         if new == long + short:
